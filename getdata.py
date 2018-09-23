25ea4f35acab765da077d44c2b200feee52a9762
easydr1 <easydr1@gmail.com>
2018-09-23 14:58:00 +0200

    Update getdata.py
    initial

--- getdata.py
+++ getdata.py
@@ -1,2 +1,19 @@
-import 
+#
+# go to your local folder
+# >git init
+#   (get the git url from GIT CLONE OR DOWNLOAD)
+# >git pull https://github.com/easydr1/getstat.git 
+# work on your files
+# >git commit (when finished)
+#
+
+fruits=["apple", "peer", "banana"]
+bigMat=range(1,100)
+def main():
+    print("Hello")
+    for matrix in bigMat:
+        print("matrix = ", matrix)
+
+if __name__=="__main__":
+    main() 
 
